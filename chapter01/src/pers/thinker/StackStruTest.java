a8fbd5173a5cacf57b3ee2cb56a45d8d5320de96
thinker <3218632496@qq.com>
2022-07-30 20:45:28 +0800

    2022/7/30-jvm学习-first commit

---
+++ chapter01/src/pers/thinker/StackStruTest.java
@@ -0,0 +1,12 @@
+package pers.thinker;
+
+/**
+ * @author thinker
+ * @date 2022/7/30 20:38
+ */
+public class StackStruTest {
+
+    public static void main(String[] args) {
+        int i = 2 + 3;
+    }
+}
